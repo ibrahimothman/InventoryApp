a967b442c1b58b773c695a709c61271495c56102
ibrahim karim othman <ibrahimothmanre@gmail.com>
2018-08-08 17:38:22 +0200

    add product detail layout

---
+++ app/src/main/java/com/example/ibrakarim/inventoryapp/ui/ProductDetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.ibrakarim.inventoryapp.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ibrakarim.inventoryapp.R;
+
+public class ProductDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_detail);
+    }
+}
